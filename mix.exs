fcf6bde83bc0c8b49853567f8959527b4dd33ece
Badu Boahen <badu.boahen@gmail.com>
2021-06-02 18:43:18 -0500

    Update elixir version to 1.11

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Safoa.MixProject do
     [
       app: :safoa,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
