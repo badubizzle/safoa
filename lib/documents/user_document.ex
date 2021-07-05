ce73301b0c869b0ddc76fcae6cc54396bc2018b3
Badu Boahen <badu.boahen@gmail.com>
2021-07-05 11:45:32 -0500

    Update user document struct with user_id and document_id fields

--- lib/documents/user_document.ex
+++ lib/documents/user_document.ex
@@ -2,11 +2,11 @@ defmodule Safoa.Documents.UserDocument do
   @moduledoc """
   Represents a user's encrypted document
   """
-  defstruct id: nil, user_key: nil
+  defstruct id: nil, user_key: nil, user_id: nil, document_id: nil
 
   def new(document_id, user_id, user_key)
       when is_binary(document_id) and is_binary(user_id) and is_binary(user_key) do
     id = "#{user_id}-#{document_id}"
-    struct!(__MODULE__, id: id, user_key: user_key)
+    struct!(__MODULE__, id: id, user_key: user_key, user_id: user_id, document_id: document_id)
   end
 end
